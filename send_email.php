bd1772e170ca26ac2a4a2e19b75a68210f8d768d
Mike Shimonov <61389419+mikeCentrol@users.noreply.github.com>
2023-08-20 14:18:16 -0400

    updated php code

--- send_email.php
+++ send_email.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = "mshimonov@gmail.com"; // Email address to receive the email
+    $to = "jshimonov@gmail.com"; // Email address to receive the email
     $subject = $_POST["event-type"]; // Event type as the subject
     $message = $_POST["comments"]; // User's comments as the email body
     $headers = "From: " . $_POST["email"] . "\r\n" .
